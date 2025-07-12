6f18a6e1fb3f09259fe4b2cbf79ed8d2e8656687
ramnivaspatel <ramnivaspatel0802@gmail.com>
2025-07-12 14:19:57 +0530

    Delete templates directory

--- templates/js/data.js
+++
@@ -1,5 +0,0 @@
-window.employeeData = [
-  { id: 1, firstName: "Ramnivas", lastName: "Patel", email: "ram@abc.com", department: "Engineering", role: "Frontend Developer" },
-  { id: 2, firstName: "Shiva", lastName: "Kumar", email: "shiv@abc.com", department: "Sales", role: "Executive" },
-  { id: 3, firstName: "Dhatta", lastName: "Rao", email: "dhatta@abc.com", department: "Marketing", role: "Manager" },
-];
